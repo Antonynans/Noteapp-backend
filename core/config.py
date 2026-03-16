c4dc3b396bb1847382cbeca3f8bbd67fa5a421e4
antony <antonynans@gmail.com>
2026-03-16 15:08:49 +0100

    created mail service

--- core/config.py
+++ core/config.py
@@ -7,6 +7,19 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 43200  # 30 days
 
+    MAIL_USERNAME: str = ""
+    MAIL_PASSWORD: str = ""
+    MAIL_FROM: str = ""
+    MAIL_PORT: int = 587
+    MAIL_SERVER: str = "smtp.gmail.com"
+    MAIL_STARTTLS: bool = True
+    MAIL_SSL_TLS: bool = False
+
+    UPLOAD_DIR: str = "uploads/avatars"
+    MAX_UPLOAD_SIZE: int = 5_000_000  # 5MB
+
+    BASE_URL: str = "http://localhost:8000"
+
     class Config:
         env_file = ".env"
 
