916f590e2c3ea72cfd5fc7ba7daa7e984fd185c9
antony <antonynans@gmail.com>
2026-03-17 17:46:22 +0100

    updated the frontend url

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["https://quillnote.netlify.app"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
