3f7b6bd69eac5c0a3d42ea7b2e7bdf803ccb95b5
antony <antonynans@gmail.com>
2026-03-20 18:26:20 +0100

    created the rate limiter function

--- core/limiter.py
+++ core/limiter.py
@@ -6,7 +6,6 @@ from fastapi.responses import JSONResponse
 
 limiter = Limiter(key_func=get_remote_address)
 
-
 def rate_limit_exceeded_handler(request: Request, exc: RateLimitExceeded):
     return JSONResponse(
         status_code=429,
