30dcae00d5a7c17897d7417ab8ac457b87b76dd8
antony <antonynans@gmail.com>
2026-03-24 17:15:09 +0100

    added sessions

--- models/user.py
+++ models/user.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, DateTime, Boolean, func
+from sqlalchemy.orm import relationship
 from db.database import Base
 
 
@@ -24,3 +25,5 @@ class User(Base):
 
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    sessions = relationship("UserSession", back_populates="user", cascade="all, delete-orphan")
